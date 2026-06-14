006705e47c5f124e652f7a9906e3a475ae2eed5a
Denis Zotov <zot.dv0@gmail.com>
2026-06-14 15:53:17 +0300

    feat(board): add game board placeholder

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
+const boardSide = 5;
+const createBoard = (boardElement: HTMLElement) => {
+    if (boardElement.childElementCount === 0) {
+        boardElement.classList.add(`board-side-${boardSide}`);
+        boardElement.style.gridTemplateColumns = `repeat(${boardSide}, 1fr)`;
+        boardElement.style.gridTemplateRows = `repeat(${boardSide}, 1fr)`;
+        for (let index = 0; index < boardSide * boardSide; ++index) {
+            const cellElement = document.createElement("div");
+            cellElement.id = "cell_" + index;
+            cellElement.className = "cell";
+            cellElement.innerText = `${index+1}`;
+            boardElement.appendChild(cellElement);
+        }
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    const hello = document.createElement('p');
-    hello.textContent = 'Script loaded!';
-    document.getElementById('app')!.appendChild(hello);
+    const boardElement = <HTMLElement> document.querySelector("#board");
+    createBoard(boardElement);
 });
