e8389a3dac856e45374ad90015b08d74dee37c62
Denis Zotov <zot.dv0@gmail.com>
2026-06-19 01:59:41 +0300

    feat(main): use game view in main script

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,10 @@
 import Game from "./game";
+import {GameHTMLView} from "./view.ts";
 
-const boardSide = 5;
-const game = new Game(boardSide * boardSide).fillBoard(false, 5,16);
-
-const createBoard = (boardElement: HTMLElement) => {
-    if (boardElement.childElementCount === 0) {
-        boardElement.classList.add(`board-side-${boardSide}`);
-        boardElement.style.gridTemplateColumns = `repeat(${boardSide}, 1fr)`;
-        boardElement.style.gridTemplateRows = `repeat(${boardSide}, 1fr)`;
-        for (let index = 0; index < game.boardSize; ++index) {
-            const cellElement = document.createElement("div");
-            cellElement.id = "cell_" + index;
-            cellElement.className = "cell";
-            boardElement.appendChild(cellElement);
-        }
-    }
-    updateBoard(boardElement);
-}
-
-const updateBoard = (boardElement: HTMLElement) => {
-    for (const cellElement of boardElement.children) {
-        if (cellElement instanceof HTMLElement) {
-            let index = +cellElement.id.replace('cell_', '');
-            let cell = game.getCell(index);
-            cellElement.innerText = (cell && cell.value) ? `${cell.value}` : '';
-        }
-    }
-}
+const rows = 5, cols = 5;
+const game = new Game(rows, cols).fillBoard(false, 5,16);
 
 document.addEventListener('DOMContentLoaded', () => {
-    const boardElement = <HTMLElement> document.querySelector("#board");
-    createBoard(boardElement);
+    let gameElement = document.getElementById('game');
+    new GameHTMLView(game).render(gameElement);
 });
